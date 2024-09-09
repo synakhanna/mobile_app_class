f463af96e89ce95d392f46d71c98ee080f59dd91
synakhanna <sk9921@nyu.edu>
2024-09-09 19:43:55 -0400

    fixing layout

--- recipe-card/src/components/RecipeCard/index.js
+++ recipe-card/src/components/RecipeCard/index.js
@@ -1,31 +1,34 @@
-import Card from './Card.js'
-import RecipeImg from './RecipeImg.js'
-import RecipeInfo from './RecipeInfo.js'
-import IngredientList from './IngredientList.js'
-import InstructionList from './InstructionList.js'
-// import RECIPE_IMG from '../../assets/pancake.png'
-import {RECIPE} from './recipe-data.js'
+import Card from './Card.js';
+import RecipeImg from './RecipeImg.js';
+import RecipeInfo from './RecipeInfo.js';
+import IngredientList from './IngredientList.js';
+import InstructionList from './InstructionList.js';
+import { RECIPE } from './recipe-data.js';
+import './styles.css';
 
 export default function RecipeCard() {
   return (
     <Card>
-      {/* RecipeImg */}
-      <div>
-        {/* <img src={RECIPE.imgSrc} alt="pancake" /> */}
-        <RecipeImg imgSrc={RECIPE.imgSrc} altText={RECIPE.title} />
-      </div>
-      {/* layout div */}
-      <div>
-        {/* RecipeInfo */}
-        <RecipeInfo title={RECIPE.title} description={RECIPE.description} />
-      </div>
-      <div>
-        {/* IngredientsList */}
-        <IngredientList ingredients={RECIPE.ingredients} />
-
-        {/* InstructionsList */}
-        <InstructionList instructions={RECIPE.instructions} />
+      <div className="recipe-card-layout">
+        {/* Recipe Image */}
+        <div className="recipe-image-container">
+          <RecipeImg imgSrc={RECIPE.imgSrc} altText={RECIPE.title} />
+        </div>
+        
+        {/* Recipe Info and Details */}
+        <div className="recipe-info-container">
+          {/* Recipe Title */}
+          <RecipeInfo title={RECIPE.title} description={RECIPE.description} />
+          
+          <div className="recipe-details">
+            {/* Ingredients */}
+            <IngredientList ingredients={RECIPE.ingredients} />
+            
+            {/* Instructions */}
+            <InstructionList instructions={RECIPE.instructions} />
+          </div>
+        </div>
       </div>
     </Card>
-  )
+  );
 }
