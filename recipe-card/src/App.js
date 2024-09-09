40618b375c305bf4a1135f83b1b546ee657ddb08
synakhanna <sk9921@nyu.edu>
2024-09-09 19:35:27 -0400

    editing recipe

--- recipe-card/src/App.js
+++ recipe-card/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import RecipeCard from './components/RecipeCard'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // adjacent element but always be wrapped in ONE parent tag
+    <>
+      <RecipeCard />
+    </>
+  )
 }
-
-export default App;
--- recipe-card/src/index.js
+++ recipe-card/src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import react and reactdom libraries for use
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import our main parent app component
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// grab and store our root div element as a var
+const root = ReactDOM.createRoot(document.getElementById('root'))
+// use react dom to render our react project inside that div
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
